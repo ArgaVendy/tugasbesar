bb44df489a2d9248ebbdbf1a302b237a7600622d
ArgaVendy <gloryan.argavendy@mhs.itenas.ac.id>
2025-01-14 17:24:52 +0700

    back end

---
+++ resources/views/pelanggan/layout/component/footer.blade.php
@@ -0,0 +1,40 @@
+<div class="d-flex justify-content-between gap-5 pt-4 mt-5">
+    <div class="title-left w-25">
+        <div class="header-title fs-4 mb-2 font-weight-bolder">
+            Ruang Mobil
+        </div>
+        {{-- <p> Jln. Perumahan De Marrakesh Blok C2 No.18</p> --}}
+    </div>
+    <div class="title-middle w-25">
+        <div class="header-title fs-4 mb-2 font-weight-bolder">
+            About Us
+        </div>
+        <ul class="list-unstyled">
+            <li>
+                <p>
+                    Gloryan ArgaVendy
+                </p>
+            </li>
+            <li>
+                <p>Amsa Tarigan</p>
+            </li>
+            <li>
+                DeanKT
+            </li>
+        </ul>
+    </div>
+    <div class="title-sosmed w-30">
+        <div class="header-title fs-4 mb-2 font-weight-bolder">
+            Social Media
+        </div>
+        <div class="sosmed d-flex gap-3">
+            <i class="fa-brands fa-instagram fa-2x"></i>
+            <i class="fa-brands fa-square-facebook fa-2x"></i>
+            <i class="fa-brands fa-whatsapp fa-2x"></i>
+            <i class="fa-brands fa-linkedin fa-2x"></i>
+        </div>
+    </div>
+</div>
+<div class="text-center p-lg-4 mt-3">
+    RUANG MOBIL</i>
+</div>
